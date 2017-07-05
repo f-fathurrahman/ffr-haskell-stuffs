72a3ad20dd9b6b4da0320f1a66f1a242aa672797
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-07-05 21:34:48 +0700

    Example of where keyword

--- varfun.hs
+++ varfun.hs
@@ -1,2 +1,23 @@
+-- Define a variable
 r = 5.0
-area = pi*r^2
+-- A variable in Haskell is immutable
+
+
+-- Define a function
+area r = pi*r^2
+-- This expression is quite similar to how the equation for
+-- calculating circle area looks like:
+-- $$A(r) = \pi r^2$$
+
+-- Some more functions:
+double x    = 2*x
+quadruple x = double (double x)
+square x    = x*x
+half x      = x/2
+
+-- Heron's formula
+heron a b c = sqrt( s*(s-a)*(s-b)*(s-c) )
+    where
+    s = (a + b + c)/2
+
+
